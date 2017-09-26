940a628f0e30a807d75dc08d467df36bd85a8041
Marc Villagrasa <marcvege@gmail.com>
2017-09-27 00:44:25 +0200

    Refactor parametrizated test

--- src/test/java/RoverTest.java
+++ src/test/java/RoverTest.java
@@ -1,12 +1,16 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 class RoverTest {
-    @Test
-    void rotate_right() {
+
+    @ParameterizedTest(name = "\"{0}\" should be {1}")
+    @CsvSource({ "R, 0:0:E"})
+    void rotate_right(String commands, String position) {
         Rover rover = new Rover();
-        assertThat(rover.move("R"), is("0:0:E"));
+        assertThat(rover.move(commands), is(position));
     }
 }
