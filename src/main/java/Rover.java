90d30e8dedee1cb890cc5665d97357a815a3e72c
Marc Villagrasa <marcvege@gmail.com>
2017-09-28 00:14:11 +0200

    Refactor simplify Compass

--- src/main/java/Rover.java
+++ src/main/java/Rover.java
@@ -1,15 +1,21 @@
 public class Rover {
-    private Compass compass = new Compass('N');
+    public static final char ROTATE_TO_RIGHT = 'R';
+    public static final char ROTATE_TO_LEFT = 'L';
+
+    private Compass compass = new Compass();
 
     public String move(String commands) {
         for (char command : commands.toCharArray()) {
-            if('R' == command) {
-                compass.rotateToRight();
-            }else if('L' == command){
-                compass.rotateToLeft();
+            switch (command) {
+                case ROTATE_TO_RIGHT:
+                    compass.rotateToRight();
+                    break;
+                case ROTATE_TO_LEFT:
+                    compass.rotateToLeft();
+                    break;
             }
         }
-        return "0:0:" + compass.getDirection();
+        return "0:0:" + compass.getCurrentDirection();
     }
 
 }
