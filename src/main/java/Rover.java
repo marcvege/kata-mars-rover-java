0b739991d7bc5ed39d23bcd3771ce04dfae5c7e5
Marc Villagrasa <marcvege@gmail.com>
2017-09-28 01:06:14 +0200

    Refactor extract Grid class

--- src/main/java/Rover.java
+++ src/main/java/Rover.java
@@ -1,10 +1,12 @@
+import java.awt.*;
+
 public class Rover {
     public static final char ROTATE_TO_RIGHT = 'R';
     public static final char ROTATE_TO_LEFT = 'L';
     public static final char MOVE_FORWARD = 'M';
 
     private Compass compass = new Compass();
-    private Position position = new Position();
+    private Grid grid = new Grid();
 
     public String move(String commands) {
         for (char command : commands.toCharArray()) {
@@ -16,10 +18,12 @@ public class Rover {
                     compass.rotateToLeft();
                     break;
                 case MOVE_FORWARD:
-                    position.moveTo(compass.getDirection());
+                    grid.moveTo(compass.getDirection());
                     break;
             }
         }
+
+        Position position = grid.getPosition();
         return position.getX() + ":" + position.getY() + ":" + compass.getDirection();
     }
 
